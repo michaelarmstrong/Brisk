37c4186db6c8c56d243ddf8f7e0180552c9ff75e
Michael Armstrong <michael.armstrong@me.com>
2016-09-24 00:11:35 +0100

    swift 2.3 changes.

---
+++ Brisk/BriskWatch/BriskWatch.h
@@ -0,0 +1,19 @@
+//
+//  BriskWatch.h
+//  BriskWatch
+//
+//  Created by Michael Armstrong on 15/12/2015.
+//  Copyright © 2015 Michael Armstrong. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for BriskWatch.
+FOUNDATION_EXPORT double BriskWatchVersionNumber;
+
+//! Project version string for BriskWatch.
+FOUNDATION_EXPORT const unsigned char BriskWatchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BriskWatch/PublicHeader.h>
+
+
